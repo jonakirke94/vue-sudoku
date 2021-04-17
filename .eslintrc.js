5ab4b1e397a180e47f40398a3495e95a7e3088f7
Jonathan <jhk@northarc.dk>
2021-04-17 16:46:21 +0200

    turned linebreak style off

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'class-methods-use-this': 'off',
-		'linebreak-style': ['error', 'windows'],
+		'linebreak-style': 'off',
 		'no-param-reassign': ['error', { props: false }],
 		'no-underscore-dangle': 'off',
 		'no-shadow': 'off',
